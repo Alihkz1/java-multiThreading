c31eebed43e589c57e3ba21adaae2a4c996eee79
Ali Hosseinkhani <aliho3einkhani1@gmail.com>
2024-07-24 13:42:14 +0330

    work on threads

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,55 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static class MyRunnable implements Runnable {
+        @Override
+        public void run() {
+            System.out.println("MyRunnable");
+        }
     }
+
+    public static class MyRunnable2 extends Thread {
+        public void run() {
+            System.out.println("MyRunnable2");
+        }
+    }
+
+    private static void myRunnable3() {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("myRunnable3");
+            }
+        };
+        Thread thread = new Thread(runnable);
+        thread.start();
+        try {
+            Thread.sleep(2000);
+            System.out.println("in myRunnable3 main thread slept 2s");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void myRunnable4() {
+        Runnable runnable = () -> {
+            while (true) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("myRunnable4");
+            }
+        };
+        Thread thread = new Thread(runnable);
+        thread.start();
+    }
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new MyRunnable());
+        myRunnable4();
+        myRunnable3();
+        thread.start();
+    }
+
 }
